a93d51c8d39481e38fcb295d609c74265721f953
timmoo <47295295+timmoo@users.noreply.github.com>
2019-02-10 21:37:58 +0300

    Delete homework1-3.sql

--- homework1-3.sql
+++
@@ -1,5 +0,0 @@
-USE WideWorldImporters;
-GO
-SELECT * FROM Purchasing.Suppliers
-LEFT OUTER JOIN Purchasing.PurchaseOrders ON Suppliers.SupplierID = PurchaseOrders.SupplierID
-where PurchaseOrders.SupplierID is NULL
